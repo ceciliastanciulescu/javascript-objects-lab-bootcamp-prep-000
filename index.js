4550c92bb8b1dc1f82544e674af0f85954f51960
Stanciulescu Cecilia Ionela <cecilia.zagrean@gmail.com>
2018-11-13 09:19:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, { [key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  recipes.key = value;
+  return recipes;
+}
